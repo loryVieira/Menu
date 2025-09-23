b73346564b7ae42d20cc277a77f17dc92457c59f
wenderson.souza <wenderson.souza273127@gmail.com>
2025-09-23 20:33:56 -0300

    Criação de telas e menu lateral

---
+++ telas/TelaInicio.js
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function TelaInicio() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Bem-vindo</Text>
+        </View>
+    );
+}
+
+export default TelaInicio;
